7f4822ad5e862711d90388271f44c6d26cdebf19
Vincent Boucheny <vincent.boucheny@powershop.co.nz>
2016-09-15 11:34:25 +1200

    Add my devtrain's hangman as dependency and plug it to HangBot. The command 'start' starts a new game and HangBot displays the masked word.

--- Gemfile
+++ Gemfile
@@ -2,4 +2,5 @@ source 'https://rubygems.org'
 
 # Specify your gem's dependencies in hangman_slack.gemspec
 gemspec
-gem 'slack-ruby-bot', git: 'git@github.com:dblock/slack-ruby-bot.git'
+gem 'slack-ruby-bot', :git => 'git@github.com:dblock/slack-ruby-bot.git'
+gem 'hangman', :path => '/Users/vincentboucheny/Documents/archive/devtrain/hangman'
--- lib/hangman_slack.rb
+++ lib/hangman_slack.rb
@@ -1,9 +1,42 @@
 require "hangman_slack/version"
 require "slack-ruby-bot"
+require "hangman"
+
+class HangBotView < Hangman::View
+  PLACEHOLDER = '?'
+
+  def initialize(engine:)
+    super(:engine => engine)
+  end
+
+  def show_state
+    build_state_string.chars.join(' ')
+  end
+end
 
 class HangBot < SlackRubyBot::Bot
-  command 'hang' do |client, data, match|
-    client.say(:text => 'in there', :channel => data.channel)
+  def initialize
+    super
+  end
+
+  def self.start
+    @validator = Hangman::CaseInsensitiveValidator.new
+    @word_picker = Hangman::WordPicker.new
+    @engine = Hangman::Engine.new(word: @word_picker.pick, lives: 5, validator: @validator)
+    @view = HangBotView.new(engine: @engine)
+  end
+
+  def self.say(text)
+    @client.say(:text => text, :channel => @data.channel)
+  end
+
+  command 'start' do |client, data, match|
+    @client = client
+    @data = data
+
+    say('Okie dokie')
+    start
+    say("The secret word is #{@view.show_state}")
   end
 end
 
