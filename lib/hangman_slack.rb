61ebfeb69ee213c2931dd3aba03ac3bf0d1cce35
Vincent Boucheny <vincent.boucheny@powershop.co.nz>
2016-09-15 16:01:02 +1200

    Add a bunch of commands and for spec (Bots are not easy to test though).

--- lib/hangman_slack.rb
+++ lib/hangman_slack.rb
@@ -2,6 +2,10 @@ require "hangman_slack/version"
 require "slack-ruby-bot"
 require "hangman"
 
+SlackRubyBot.configure do |config|
+  config.aliases = ['hg', 'hang']
+end
+
 class HangBotView < Hangman::View
   PLACEHOLDER = '?'
 
@@ -14,16 +18,22 @@ class HangBotView < Hangman::View
   end
 end
 
-class HangBot < SlackRubyBot::Bot
-  def initialize
-    super
+class HangBotWordPicker
+  def pick
+    ENV['SECRET_WORD'] || Hangman::WordPicker.new.pick
   end
+end
+
+class HangBot < SlackRubyBot::Bot
+  @@leaderboard = {}
 
   def self.start
+    say("Let's start a game")
     @validator = Hangman::CaseInsensitiveValidator.new
-    @word_picker = Hangman::WordPicker.new
+    @word_picker = HangBotWordPicker.new
     @engine = Hangman::Engine.new(word: @word_picker.pick, lives: 8, validator: @validator)
     @view = HangBotView.new(engine: @engine)
+    #    show_state
   end
 
   def self.say(text)
@@ -32,51 +42,111 @@ class HangBot < SlackRubyBot::Bot
 
   def self.show_state
     say("#{@view.show_state} (#{@engine.lives} lives remaining)")
+    say("Already guessed #{@validator.guessed_letters.to_a.join(', ')}")
+  end
+
+  def self.has_game?
+    !@engine.nil? && !@engine.game_over?
   end
 
   def self.game_started?
-    @engine.nil? || @engine.game_over?
+    say("There's no game going on") unless res = has_game?
+    res
   end
 
-  command 'start' do |client, data, match|
-    @client = client
-    @data = data
-
-    start
-    show_state
+  def self.no_game?
+    say("A game has already started") unless res = !has_game?
+    res
   end
 
-  command 'guess' do |client, data, match|
-    @client = client
-    @data = data
-    guess = match[:expression]
-
-    if game_started?
-      say("There's no game going on")
-    elsif !@view.input_sane?(guess)
-      say("'#{guess}' is not a valid input.")
-    else
-      @engine.guess(guess)
-      if @engine.win?
-        say("You've won! The word was '#{@engine.word}'")
-      elsif @engine.lost?
-        say("You've lost! The word was '#{@engine.word}'")
-      else
-        show_state
-      end
-    end
+  def self.valid_input?(guess)
+    say("'#{guess}' is not a valid input") unless res = @view.input_sane?(guess)
+    res
   end
 
-  command 'state' do |client, data, match|
+  def self.start_command(client, data)
     @client = client
     @data = data
+  end
 
-    if game_started?
-      say("There's no game going on")
+  def self.init_user_score(user)
+    @@leaderboard[user] = 0 unless @@leaderboard.has_key?(user)
+  end
+
+  def self.correct_guess(user)
+    say("GG <@#{user}>, you've guessed correctly")
+    @@leaderboard[user] += 2
+  end
+
+  def self.incorrect_guess(user)
+    say("Too bad <@#{user}>, you've guessed poorly")
+    @@leaderboard[user] -= 1
+  end
+
+  def self.show_game_state
+    if @engine.win?
+      say("You've won! The word was '#{@engine.word}'")
+      @@leaderboard[data.user] += @engine.word.length
+    elsif @engine.lost?
+      say("You've lost! The word was '#{@engine.word}'")
     else
       show_state
     end
   end
+
+  command 'start' do |client, data, match|
+    start_command(client, data)
+
+    start if no_game?
+  end
+
+  command 'guess' do |client, data, match|
+    start_command(client, data)
+    guess = match[:expression]
+
+    if game_started? && valid_input?(guess)
+      user = data.user
+      init_user_score(user)
+
+      @engine.guess(guess) ? correct_guess(user) : incorrect_guess(user)
+      show_game_state
+    end
+  end
+
+  command 'state' do |client, data, match|
+    start_command(client, data)
+
+    show_state if game_started?
+  end
+
+  command 'word' do |client, data, match|
+    start_command(client, data)
+    word = match[:expression]
+
+    if game_started?
+      user = data.user
+      init_user_score(user)
+
+      @engine.guess_word(word) ? correct_guess(user) : incorrect_guess(user)
+      show_game_state
+    end
+  end
+
+  command 'scores' do |client, data, match|
+    start_command(client, data)
+
+    @@leaderboard.each do |key, value|
+      say("<@#{key}> has #{value} point(s).")
+    end
+  end
+
+  command 'reset' do |client, data, match|
+    start_command(client, data)
+
+    @engine = @validator = @view = nil
+    @@leaderboard = {}
+    say("Back to zero")
+  end
 end
 
-HangBot.run
+#HangBot.run
--- spec/hangman_slack_spec.rb
+++ spec/hangman_slack_spec.rb
@@ -1,11 +1,55 @@
 require 'spec_helper'
 
+require 'spec_helper'
+
 describe HangmanSlack do
   it 'has a version number' do
     expect(HangmanSlack::VERSION).not_to be nil
   end
 
-  it 'does something useful' do
-    expect(false).to eq(true)
+  before do
+    ENV['SECRET_WORD'] = "PowerForm"
+  end
+
+  after do
+    expect(message: "#{SlackRubyBot.config.user} reset").to respond_with_slack_message('Back to zero')
+  end
+
+  it 'says hi' do
+    expect(message: "#{SlackRubyBot.config.user} hi").to respond_with_slack_message('Hi <@user>!')
+  end
+
+  context "no game is going on" do
+    describe "start" do
+      it 'starts a new game' do
+        expect(message: "#{SlackRubyBot.config.user} start").to respond_with_slack_message("Let's start a game")
+      end
+    end
+
+    describe "guess" do
+      it "err" do
+        expect(message: "#{SlackRubyBot.config.user} guess e").to respond_with_slack_message("There's no game going on")
+      end
+    end
+  end
+
+  context "a game is going on" do
+    before do
+      expect(message: "#{SlackRubyBot.config.user} start").to respond_with_slack_message("Let's start a game")
+    end
+
+    describe "start" do
+      it "doesn't start a game twice" do
+        expect(message: "#{SlackRubyBot.config.user} start").to respond_with_slack_message("A game has already started")
+      end
+    end
+
+    describe "guess" do
+      it "err on bad input" do
+        %w{1 ! qw}.each do |input|
+          expect(message: "#{SlackRubyBot.config.user} guess #{input}").to respond_with_slack_message("'#{input}' is not a valid input")
+        end
+      end
+    end
   end
 end
